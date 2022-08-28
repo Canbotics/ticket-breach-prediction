35e24750e85f1b35482557c9cae8e170325d00c3
Matthew Moore <matthew@ocicat.media>
2022-08-28 11:39:36 -0400

    Roughed in projection structure

--- static/scripts.js
+++ static/scripts.js
@@ -1,12 +1,9 @@
 import { Component } from './scripts/component.js';
 
-import { Analyst } from './scripts/analyst.js';
-import { Day } from './scripts/day.js';
-
+import { Projection } from './scripts/projection.js';
 
 document.addEventListener('DOMContentLoaded', (event) => {
     Component.init();
 
-    Analyst.init();
-    Day.init();
+    Projection.init();
 });
