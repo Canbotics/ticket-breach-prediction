55abfeafc0d64e2c5c0cb6016f0d212f7ff72d3f
Matthew Moore <matthew@ocicat.media>
2022-08-29 12:28:22 -0400

    Moved Day and analyst init() into global scripts

--- static/scripts.js
+++ static/scripts.js
@@ -1,9 +1,14 @@
 import { Component } from './scripts/component.js';
 
+import { Analyst } from './scripts/analyst.js';
+import { Day } from './scripts/day.js';
 import { Projection } from './scripts/projection.js';
 
 document.addEventListener('DOMContentLoaded', (event) => {
     Component.init();
+
+    Analyst.init();
+    Day.init();
 
     Projection.init();
 });
