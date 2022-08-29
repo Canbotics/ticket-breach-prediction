ab7880907954c876e9066ce88c52b7c2b7665753
Matthew Moore <matthew@ocicat.media>
2022-08-29 12:27:56 -0400

    Modified Analyst to handle adding projections - New static variables - Moved some init() logic to new populateAnalysts() method - Added methods for handling dynamically adding new form fields

--- static/scripts/analyst.js
+++ static/scripts/analyst.js
@@ -1,10 +1,30 @@
+import { Template } from "./template.js";
+
 export class Analyst {
+    static addAnalyst;
+    static templateAddAnalyst;
+
     constructor(analyst) {
         this.name = analyst.name;
         this.ticketsPerDay = analyst.ticketsPerDay;
     }
 
-    static init(analysts) {
+    static init() {
+        this.templateAddAnalyst = Template.get('add-analyst-li');
+        this.addAnalyst = document.getElementById('form-add-analyst');
+        
+        const addButton = this.addAnalyst.querySelector('.add');
+
+        addButton.addEventListener('click', (event) => {
+            const count = parseInt(addButton.dataset.count);
+
+            this.formAddAnalyst(count);
+
+            addButton.dataset.count = count + 1;
+        });
+    }
+
+    static populateAnalysts(analysts) {
         const newAnalysts = [];
 
         analysts.forEach((analyst) => {
@@ -12,5 +32,15 @@ export class Analyst {
         });
 
         return newAnalysts;
+    }
+
+    static formAddAnalyst(count) {
+        const analysts = ['Michael','Matthew','Laura','Allison','Sam',];
+        const li = Template.clone(this.templateAddAnalyst);
+        const name = li.querySelector('.name');
+
+        name.value = analysts[count] || "";
+
+        this.addAnalyst.append(li);
     }
 }
